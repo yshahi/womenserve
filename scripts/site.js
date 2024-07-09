795e1e8674b1c5de77e26164ff168f08ee01b422
sp <squarespace@Yubrajs-Mac-mini.local>
2024-07-09 15:48:55 +0545

    Contact, donate pages

--- scripts/site.js
+++ scripts/site.js
@@ -19,12 +19,42 @@ document.addEventListener('DOMContentLoaded', loadAllImages);
 
 
 /* Hamburger menu */
-document.addEventListener("DOMContentLoaded", function () {
-  function toggleMenu() {
-    var navMenu = document.getElementById('nav-menu');
-    var body = document.body;
-      
-    navMenu.classList.toggle('active');
+
+/*function toggleMenu() {
+  const navMenu = document.getElementById('nav-menu');
+  const body = document.body;
+    
+  navMenu.classList.toggle('active');
+  body.classList.toggle('no-scroll');
+};*/
+document.addEventListener('DOMContentLoaded', () => {
+  const menuBtn = document.querySelector('.menu-btn');
+  const nav = document.querySelector('.nav');
+  const overlay = document.querySelector('.overlay');
+  const body = document.body;
+
+  menuBtn.addEventListener('click', (e) => {
+    e.stopPropagation();  // Prevent the click event from bubbling up to the document
+    menuBtn.classList.toggle('open');
+    nav.classList.toggle('open');
+    overlay.classList.toggle('open');
     body.classList.toggle('no-scroll');
+  });
+
+  overlay.addEventListener('click', () => {
+    closeMenu();
+  });
+
+  document.addEventListener('click', (e) => {
+    if (nav.classList.contains('open') && !menuBtn.contains(e.target)) {
+      closeMenu();
+    }
+  });
+
+  function closeMenu() {
+    menuBtn.classList.remove('open');
+    nav.classList.remove('open');
+    overlay.classList.remove('open');
+    body.classList.remove('no-scroll');
   }
 });
